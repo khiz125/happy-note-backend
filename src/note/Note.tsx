b94a5d47bb2548d545001b6a14aa7d2a7bd0e9a2
khiz125 <ken.hiro118@gmail.com>
2023-05-24 22:37:41 +0900

    add HappyList.tsx

--- src/note/Note.tsx
+++ src/note/Note.tsx
@@ -1,24 +1,55 @@
-import React from 'react'
+import React from "react";
+import HappyList from "./HappyList";
 
 const note = () => {
 
   
   return (
-    <main className='box-border text-gray-600 w-full'>
-      <section className='flex items-center justify-center m-10'>
+    <main className='box-border text-gray-600 w-full px-20'>
+      <section className='flex items-center justify-center m-10 px-40'>
         <h1>
           Happy note
         </h1>
       </section>
       <section className='flex flex-col items-center justify-center m-10'>
         <h3>今日のハッピーノート</h3>
-        <div className='border border-gray-300 shadow-xl rounded-[20px]'>
-          <textarea className='w-full rounded-[20px]' name="note" id="" cols={50} rows={10} />
+        <h3 className="text-left w-4/5 mt-4">今日の出来事でハッピーだったことは…</h3>
+        <div className='shadow-xl rounded-[20px] border border-gray-200'>
+          <textarea 
+            className='relative hidden-scrollbar text-[#8A8684]  
+            border-gray-200 outline-none focus:border-[#D9D9D9] w-full p-4 rounded-[20px]' 
+            name="note" 
+            id="" 
+            cols={40} 
+            rows={2} 
+          />
+        </div>
+        <h3 className="text-left w-4/5 mt-4">今日の出来事で感謝したいことは…</h3>
+        <div className='shadow-xl rounded-[20px] border border-gray-200'>
+          <textarea 
+            className='relative hidden-scrollbar text-[#8A8684]  
+            border-gray-200 outline-none focus:border-[#D9D9D9] w-full p-4 rounded-[20px]' 
+            name="note" 
+            id="" 
+            cols={40} 
+            rows={2} 
+          />
+        </div>
+        <h3 className="text-left w-4/5 mt-4">今日の出来事で達成できたことは…</h3>
+        <div className='shadow-xl rounded-[20px] border border-gray-200'>
+          <textarea 
+            className='relative hidden-scrollbar text-[#8A8684]  
+            border-gray-200 outline-none focus:border-[#D9D9D9] w-full p-4 rounded-[20px]' 
+            name="note" 
+            id="" 
+            cols={40} 
+            rows={2} 
+          />
         </div>
       </section>
       <section className='flex flex-col items-center justify-center m-10'>
         <h3>これまでハッピーだったことリスト</h3>
-
+        <HappyList />
       </section>
     </main>
   )
