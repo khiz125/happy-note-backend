a482ff9e5cdaddbc6f4efa9a0ce66bc95fc6ebec
khiz125 <ken.hiro118@gmail.com>
2023-05-28 14:59:29 +0900

    add button style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,32 @@
 import React from "react"
 
-const Button = () => {
-  (
-    <div>Button</div>
-  )
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  addClassName?: string;
+  variant?:
+    | "primary"
+    | "light-gray"
 }
+const Button = ({
+  className,
+  children,
+  variant = "primary",
+  ...props
+}: ButtonProps) => {
+  let btnStyle;
+  switch (variant) {
+    case "primary":
+      btnStyle = `border-gray-200 bg-green-300 py-3 w-full border-2 rounded-[20px] text-white hover:bg-[#61C697] hover:border-[#61C697]`;
+      break;
+    case "light-gray":
+      btnStyle = `bg-[#BABABA] py-3 w-full rounded-[20px] text-white`;
+      break;
+  }
+
+  return (
+    <button className={className || btnStyle} {...props}>
+      {children}
+    </button>
+  );
+};
+
 export default Button;
