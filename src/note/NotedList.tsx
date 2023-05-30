712002874af921a4526dc3bd940fea4b70f3f2ca
khiz125 <ken.hiro118@gmail.com>
2023-05-30 22:49:37 +0900

    add GET method and POST method, installed axios

--- src/note/NotedList.tsx
+++ src/note/NotedList.tsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useContext } from "react"
+import axios from 'axios';
+import { NotesContext } from "./Note";
 
 interface NotedListProps {
   name: string;
@@ -6,14 +8,29 @@ interface NotedListProps {
 
 const NotedList: React.FC<NotedListProps> = ({ name }) => {
 
-  const [notedList, setNotedList] = useState<string[]>(["happy"]);
+  const { isAdded } = useContext(NotesContext);
+  const [notedList, setNotedList] = useState<Record<string, any>[]>([]);
+
+  useEffect(() => {
+    const url = `${process.env.REACT_APP_HARPERDB_CUSTOM_FUNCTIONS_URL}/happynote/notes/list/`;
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        setNotedList(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [isAdded])
 
   return (
     <div>
-      {notedList.map((note: string) => {
+      {notedList.map((note: Record<string, any>) => {
+        if (name === note.category)
         return (
           <ul>
-            <li className="list-disc list-inside pl-10 my-4">{note}</li>
+            <li className="list-disc list-inside pl-10 my-4">{note.text}</li>
           </ul>
         )
       })}
