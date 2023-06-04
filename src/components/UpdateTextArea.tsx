a90a2e8a9d7dfbb8e943fce6df5f25b41a997644
khiz125 <ken.hiro118@gmail.com>
2023-06-04 15:59:58 +0900

    add put method to edit text

--- src/components/UpdateTextArea.tsx
+++ src/components/UpdateTextArea.tsx
@@ -11,7 +11,7 @@ interface UpdateTextAreaProps {
   note: Record<string, any>;
 }
 
-const UpdateTextArea: React.FC<UpdateTextAreaProps> = ({ 
+const UpdateTextArea: React.FC<UpdateTextAreaProps> = ({
   name,
   addClassName,
   defaultValue,
@@ -22,21 +22,21 @@ const UpdateTextArea: React.FC<UpdateTextAreaProps> = ({
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setIsEditing(false);
     const text = ref.current?.value;
 
     if (text) {
       const data = {
         id: note.id,
-        category: note.name,
+        category: name,
         text: text
       }
 
       const url = `${process.env.REACT_APP_HARPERDB_CUSTOM_FUNCTIONS_URL}/happynote/notes/update/`;
 
       try {
-        const results = await axios.post(url, data);
+        const results = await axios.put(url, data);
         console.log(results);
+        setIsEditing(false);
       } catch (error) {
         console.error(error);
       }
@@ -44,29 +44,30 @@ const UpdateTextArea: React.FC<UpdateTextAreaProps> = ({
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="flex mb-2">
-        <div className='shadow-xl rounded-[20px] border border-gray-200 w-full mr-10'>
-          <textarea
-            className='relative hidden-scrollbar text-[#8A8684] min-w-[300px]
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="flex mb-2">
+          <div className='shadow-xl rounded-[20px] border border-gray-200 w-full mr-10'>
+            <textarea
+              className='relative hidden-scrollbar text-[#8A8684] min-w-[300px]
             border-gray-200 outline-none focus:border-[#D9D9D9] w-full p-4 rounded-[20px]'
-            name={`${name}`}
-            ref={ref}
-            id=""
-            cols={40}
-            rows={2}
-            defaultValue={defaultValue}
-          />
+              name={`${name}`}
+              ref={ref}
+              id=""
+              cols={40}
+              rows={2}
+              defaultValue={defaultValue}
+            />
+          </div>
+          <Button
+            type="submit"
+            addClassName={`min-w-[80px] p-2 h-[50px] ${addClassName}`}
+          >
+            保存
+          </Button>
         </div>
-        <Button
-          addClassName={`min-w-[80px] p-2 h-[50px] ${addClassName}`}
-          type="submit"
-          onClick={() => setIsEditing(false)}
-        >
-          保存
-        </Button>
-      </div>
-    </form>
+      </form>
+    </div>
   )
 }
 
